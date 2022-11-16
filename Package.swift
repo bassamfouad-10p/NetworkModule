9542919b5721fb7d505adda12fb535da61bd93b8
Bassam <bassam.fouad@10pearls.com>
2022-11-17 02:22:32 +0500

    network service added

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkModule",
+    platforms: [
+            .macOS(.v10_12),
+            .iOS(.v10),
+            .tvOS(.v10),
+            .watchOS(.v3)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,15 +18,14 @@ let package = Package(
             targets: ["NetworkModule"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git",from: "5.6.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NetworkModule",
-            dependencies: []),
+            dependencies: ["Alamofire"]),
         .testTarget(
             name: "NetworkModuleTests",
             dependencies: ["NetworkModule"]),
---
+++ Sources/NetworkModule/NetworkService/NetworkService.swift
@@ -0,0 +1,73 @@
+import Foundation
+import Alamofire
+
+enum NetworkError: Error {
+    
+    case transportError(Error)
+    case serverError(Int)
+    case noData
+    case decodingError(Error)
+}
+
+protocol NetworkService {
+    
+    func execute<Model: Decodable>(url: URL, completion: @escaping ((Result<Model, Error>) -> Void))
+}
+
+struct NetworkSerivceImpl: NetworkService {
+    
+    let session: URLSession
+    let decoder: JSONDecoder
+    
+    func execute<Model: Decodable>(url: URL, completion: @escaping ((Result<Model, Error>) -> Void)) {
+        
+        session.dataTask(with: url) { data , response, error in
+            
+            if let error = error {
+                
+                completion(.failure(NetworkError.transportError(error)))
+                return
+            }
+            if let response = response as? HTTPURLResponse, !(200..<300).contains(response.statusCode) {
+                
+                completion(.failure(NetworkError.serverError(response.statusCode)))
+                return
+            }
+            guard let data = data else {
+                
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            do {
+                
+                let model = try decoder.decode(Model.self, from: data)
+                completion(.success(model))
+                
+            } catch let error {
+                
+                completion(.failure(NetworkError.decodingError(error)))
+            }
+        }
+        .resume()
+    }
+}
+
+struct AFNetworkServiceImpl: NetworkService {
+    
+    func execute<Model>(url: URL, completion: @escaping ((Result<Model, Error>) -> Void)) where Model : Decodable {
+        
+        let request = AF.request(url)
+        request.responseDecodable(of: Model.self) { dataResponse in
+            
+            switch dataResponse.result {
+                
+            case .success(let model):
+                completion(.success(model))
+            case .failure(let afError):
+                if let error = afError as Error? {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+}
